1cc7c86c4cb779ee4da4fc398062e2d5fd107b3b
HYEJIN YUN <sena@HYEJINs-MacBook-Pro.local>
2025-03-15 13:05:23 +0700

    state

--- main.py
+++ main.py
@@ -76,9 +76,7 @@ def root():
     return {"message": "Welcome to the Cooking recipes API!"}
 
 @app.get("/auth")
-def github_login(state: str=None):
-    if state is None:
-        state = str(uuid.uuid4())
+def github_login(state: str):
     github_auth_url = (
         f"https://github.com/login/oauth/authorize?client_id={GITHUB_CLIENT_ID}&redirect_uri={OpenAI_redirectURI}&scope=read:user&state={state}"
     )
